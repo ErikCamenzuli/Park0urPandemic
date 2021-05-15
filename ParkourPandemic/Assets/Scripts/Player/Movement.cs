25debcb26a1626cc475dede7712bd239f4b12d36
Jackson Adenan <iambatby@gmail.com>
2021-05-15 20:29:54 +1000

    Movement.cs Cleanup

--- ParkourPandemic/Assets/Scripts/Player/Movement.cs
+++ ParkourPandemic/Assets/Scripts/Player/Movement.cs
@@ -1,43 +1,26 @@
 ﻿using UnityEngine;
-using MilkShake;
 
 public class Movement : MonoBehaviour
 {
-    private Rigidbody rb;
-    public Transform orientation;
+    Rigidbody rb;
+    Vector3 wallRunDir;
 
+    [Header("Checks")]
     public bool isGrounded;
-    public bool isWallRun;
-    bool isWallRunning;
-    bool isLedgeGrabbable;
-    bool isLooking;
+    public bool isWallRunning;
 
-    public GameObject wallObject;
-
+    [Header("Stats")]
     public float speed = 10f;
     public float jump = 3f;
     public float wallRunForce;
-    public float maxRunTimeWall;
-    public float maxSpeedWall;
-    public float cameraTiltMax;
-    public float cameraTiltWallRun;
 
-    public float climbSpeed;
-    public float climbWall;
-    public float distanceWallclimb;
-
+    [Header("Audio")]
     public AudioSource runningAudio;
     public AudioSource jumpingAudio;
     public AudioSource thudAudio;
 
+    [Header("Misc.")]
     public LayerMask wallRunMask;
-
-    public Shaker shaker;
-    public ShakePreset shakerPreset;
-
-    public Vector3 tempPoint;
-
-    private Vector3 wallRunDir;
 
     void Start()
     {
@@ -46,9 +29,11 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
+        CanWallRun();
         float x = Input.GetAxisRaw("Horizontal") * speed;
         float y = Input.GetAxisRaw("Vertical") * speed;
 
+        //Audio
         if (isGrounded && rb.velocity.magnitude > 5 && rb.velocity.y == 0)
             runningAudio.enabled = true;
         else
@@ -58,43 +43,33 @@ public class Movement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && (isGrounded))
         {
             rb.velocity = new Vector3(rb.velocity.x, jump, rb.velocity.z);
-            //rb.AddForce(new Vector3(rb.velocity.x, jump, rb.velocity.z));
             jumpingAudio.Play();
             isGrounded = false;
             rb.useGravity = true;
         }
 
-        isWallRun = (CanWallRun() && y > 0f);
-
+        //Wall Run
         if (CanWallRun() && y > 0f)
         {
             rb.useGravity = false;
-            //rb.velocity = Vector3.zero;
             Vector3 moveDir = (transform.forward * y).normalized;
-            rb.velocity = wallRunDir /** Vector3.Dot(moveDir, wallRunDir)*/ * wallRunForce * Time.deltaTime;
+            rb.velocity = wallRunDir * Vector3.Dot(moveDir, wallRunDir) * wallRunForce * Time.deltaTime;
         }
+
+        //Run
         else
         {
             Vector3 movePos = transform.right * x + transform.forward * y;
             Vector3 newPos = new Vector3(movePos.x, rb.velocity.y, movePos.z);
             rb.useGravity = true;
             rb.velocity = newPos;
-            //rb.AddForce(newPos);
         }
 
     }
 
     bool CanWallRun()
     {
-
-        // check if on ground
-        RaycastHit groundhit;
-        if (isGrounded)
-        {
-            return false;
-        }
-
-        // check if there's a wall to the right
+        //Left Wall Check
         if (Input.GetAxisRaw("Horizontal") > 0f)
         {
             RaycastHit rightHit;
@@ -104,12 +79,13 @@ public class Movement : MonoBehaviour
                 {
                     Debug.Log(rightHit.normal);
                     wallRunDir = Vector3.Cross(rightHit.normal, Vector3.up);
+                    isWallRunning = true;
                     return true;
                 }
             }
         }
 
-        // check if there's a wall to the left
+        //Right Wall Check
         if (Input.GetAxisRaw("Horizontal") < 0f)
         {
             RaycastHit leftHit;
@@ -118,22 +94,17 @@ public class Movement : MonoBehaviour
                 if (Vector3.Dot(leftHit.normal, transform.right) > 0.8f)
                 {
                     wallRunDir = Vector3.Cross(leftHit.normal, Vector3.up);
+                    isWallRunning = true;
                     return true;
                 }
 
             }
         }
+        isWallRunning = false;
         return false;
     }
 
-    private void OnCollisionExit(Collision other)
-    {
-        ContactPoint point = other.GetContact(0);
-        if (point.normal.y > 0.1f)
-        {
-            isGrounded = false;
-        }
-    }
+    //Ground Check
     private void OnCollisionStay(Collision other)
     {
         ContactPoint point = other.GetContact(0);
